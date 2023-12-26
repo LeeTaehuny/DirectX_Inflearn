8df3fa2a77e53a92ee4d8d059ca6ca26558b677e
LeeTaes <apth1023@naver.com>
2023-12-26 20:13:39 +0900

    10 - 12 Intersection(수학)

--- GameCoding3D/Engine/MathUtils.cpp
+++ GameCoding3D/Engine/MathUtils.cpp
@@ -214,3 +214,271 @@ Point3D MathUtils::ClosestPoint(const Ray3D& ray, const Point3D& point)
 	// 위치를 반환합니다.
 	return ray.origin + (ray.direction * t);
 }
+
+bool MathUtils::SphereSphere(const Sphere3D& s1, const Sphere3D& s2)
+{
+	// s1에서 s2까지의 거리를 구해줍니다.
+	float sqDistance = (s1.position - s2.position).LengthSquared();
+
+	// s1의 반지름과 s2의 반지름을 합해줍니다.
+	float sum = s1.radius + s2.radius;
+
+	// 반지름의 합이 거리보다 크다면 충돌한 것을 의미합니다.
+	// * 제곱 환경
+	return sqDistance <= sum * sum;
+}
+
+bool MathUtils::SphereAABB(const Sphere3D& s, const AABB3D& aabb)
+{
+	// 구의 중심과 AABB까지의 가장 가까운 점을 구해줍니다.
+	Vec3 closestPt = ClosestPoint(aabb, s.position);
+
+	// 구의 중심과 closestPt의 거리를 구해줍니다.
+	float distancePt = (closestPt - s.position).LengthSquared();
+
+	// 구의 반지름과 비교해서 반지름보다 거리가 작다면 충돌한 것을 의미합니다.
+	// * 제곱 환경
+	return distancePt <= s.radius * s.radius;
+}
+
+bool MathUtils::SphereOBB(const Sphere3D& s, const OBB3D& obb)
+{
+	// 구의 중심과 OBB까지의 가장 가까운 점을 구해줍니다.
+	Vec3 closestPt = ClosestPoint(obb, s.position);
+
+	// 구의 중심과 closestPt의 거리를 구해줍니다.
+	float distancePt = (closestPt - s.position).LengthSquared();
+
+	// 구의 반지름과 비교해서 반지름보다 거리가 작다면 충돌한 것을 의미합니다.
+	// * 제곱 환경
+	return distancePt <= s.radius * s.radius;
+}
+
+bool MathUtils::SpherePlane(const Sphere3D& s, const Plane3D& plane)
+{
+	// 구의 중심과 Plane까지의 가장 가까운 점을 구해줍니다.
+	Vec3 closestPt = ClosestPoint(plane, s.position);
+
+	// 구의 중심과 closestPt의 거리를 구해줍니다.
+	float distancePt = (closestPt - s.position).LengthSquared();
+
+	// 구의 반지름과 비교해서 반지름보다 거리가 작다면 충돌한 것을 의미합니다.
+	// * 제곱 환경
+	return distancePt <= s.radius * s.radius;
+}
+
+bool MathUtils::AABBAABB(const AABB3D& aabb1, const AABB3D& aabb2)
+{
+	// 각 AABB에 대한 Min, Max를 구해줍니다.
+	Point3D aMin = AABB3D::GetMin(aabb1);
+	Point3D aMax = AABB3D::GetMax(aabb1);
+	Point3D bMin = AABB3D::GetMin(aabb2);
+	Point3D bMax = AABB3D::GetMax(aabb2);
+
+	// x, y, z 축에 대해 비교를 진행합니다.
+	// * 3축이 전부 겹쳐있는 경우만 충돌이 발생합니다.
+	bool result =
+		(aMin.x <= bMax.x && aMax.x >= bMin.x) && // x축 겹침 비교
+		(aMin.y <= bMax.y && aMax.y >= bMin.y) && // y축 겹침 비교
+		(aMin.z <= bMax.z && aMax.z >= bMin.z);	  // z축 겹침 비교
+
+	return result;
+}
+
+Interval3D MathUtils::GetInterval(const AABB3D& aabb, const Vec3& axis)
+{
+	// Min, Max 위치를 가져옵니다.
+	Vec3 i = AABB3D::GetMin(aabb);
+	Vec3 a = AABB3D::GetMax(aabb);
+
+	// Min, Max에 따른 정점들의 위치를 구해줍니다. (8개)
+	Vec3 vertex[8] =
+	{
+		Vec3(i.x, a.y, a.z),
+		Vec3(i.x, a.y, i.z),
+		Vec3(i.x, i.y, a.z),
+		Vec3(i.x, i.y, i.z),
+
+		Vec3(a.x, a.y, a.z),
+		Vec3(a.x, a.y, i.z),
+		Vec3(a.x, i.y, a.z),
+		Vec3(a.x, i.y, i.z)
+	};
+
+	// 축에 따른 최대 최소값을 저장할 변수를 선언하고, 초기화합니다.
+	Interval3D result;
+	result.min = result.max = axis.Dot(vertex[0]);
+
+	// 모든 정점을 순회하며 Min, Max를 구해줍니다.
+	for (int i = 1; i < 8; i++)
+	{
+		// 해당 축으로 투영시킨 결과를 구해줍니다.
+		float projection = axis.Dot(vertex[i]);
+		// min, max를 비교해주도록 합니다.
+		result.min = min(result.min, projection);
+		result.max = max(result.max, projection);
+	}
+
+	return result;
+}
+
+Interval3D MathUtils::GetInterval(const OBB3D& obb, const Vec3& axis)
+{
+	// 정점의 위치들을 저장하기 위한 변수를 배열을 생성합니다.
+	Vec3 vertex[8];
+
+	// half size와 Center를 저장합니다.
+	Vec3 C = obb.position;	// Center(위치)
+	Vec3 E = obb.size;		// Extents
+
+	// obb를 기준으로 하는 3개의 축 정보를 저장합니다. (정점들의 좌표 연산용)
+	vector<Vec3> A;
+	A.push_back(obb.orientation.Right());	 // x축
+	A.push_back(obb.orientation.Up());		 // y축
+	A.push_back(obb.orientation.Backward()); // z축
+
+	// 각 정점들의 좌표를 구해줍니다.
+	vertex[0] = C + (A[0] * E.x) + (A[1] * E.y) + (A[2] * E.z);
+	vertex[1] = C - (A[0] * E.x) + (A[1] * E.y) + (A[2] * E.z);
+	vertex[2] = C + (A[0] * E.x) - (A[1] * E.y) + (A[2] * E.z);
+	vertex[3] = C + (A[0] * E.x) + (A[1] * E.y) - (A[2] * E.z);
+	vertex[4] = C - (A[0] * E.x) - (A[1] * E.y) - (A[2] * E.z);
+	vertex[5] = C + (A[0] * E.x) - (A[1] * E.y) - (A[2] * E.z);
+	vertex[6] = C - (A[0] * E.x) + (A[1] * E.y) - (A[2] * E.z);
+	vertex[7] = C - (A[0] * E.x) - (A[1] * E.y) + (A[2] * E.z);
+
+	// 축에 따른 최대 최소값을 저장할 변수를 선언하고, 초기화합니다.
+	Interval3D result;
+	result.min = result.max = axis.Dot(vertex[0]);
+
+	// 모든 정점을 순회하며 Min, Max를 구해줍니다.
+	for (int i = 1; i < 8; i++)
+	{
+		// 해당 축으로 투영시킨 결과를 구해줍니다.
+		float projection = axis.Dot(vertex[i]);
+		// min, max를 비교해주도록 합니다.
+		result.min = min(result.min, projection);
+		result.max = max(result.max, projection);
+	}
+
+	return result;
+}
+
+bool MathUtils::OverlapOnAxis(const AABB3D& aabb, const OBB3D& obb, const Vec3& axis)
+{
+	// AABB를 axis 축에 투영시킨 결과를 저장합니다.
+	Interval3D a = GetInterval(aabb, axis);
+	// OBB를 axis 축에 투영시킨 결과를 저장합니다.
+	Interval3D b = GetInterval(obb, axis);
+
+	// 투영한 결과인 a, b가 서로 겹치는지 확인합니다.
+	return ((b.min <= a.max) && (a.min <= b.max));
+}
+
+bool MathUtils::AABBOBB(const AABB3D& aabb, const OBB3D& obb)
+{
+	// 충돌을 테스트 하기 위한 분리 축 15개(aabb 3개, obb 3개, aabb와 obb의 외적 9개)를 저장합니다.
+	// * 우선은 초기값으로 aabb 3개, obb 3개의 축만 넣어주도록 합니다.
+	Vec3 test[15] =
+	{
+		Vec3(1, 0, 0),	 // AABB Axis
+		Vec3(0, 1, 0),	 // AABB Axis
+		Vec3(0, 0, 1),	 // AABB Axis
+
+		obb.orientation.Right(),	// OBB Axis
+		obb.orientation.Up(),		// OBB Axis
+		obb.orientation.Backward(),	// OBB Axis
+	};
+
+	// 나머지 축은 AABB의 축과 OBB의 축을 Cross(외적)하여 구해줍니다.
+	for (int i = 0; i < 3; i++)
+	{
+		test[6 + i * 3 + 0] = test[i].Cross(test[0]);
+		test[6 + i * 3 + 1] = test[i].Cross(test[1]);
+		test[6 + i * 3 + 2] = test[i].Cross(test[2]);
+	}
+
+	// 찾은 15개의 축들을 순회하며 분리축이 존재하는지 체크합니다.
+	for (int i = 0; i < 15; i++)
+	{
+		// 분리축(Interval이 떨어져 있는 축)이 존재한다면 충돌하지 않은 것을 의미합니다.
+		if (OverlapOnAxis(aabb, obb, test[i]) == false)
+		{
+			return false;
+		}
+	}
+
+	// 분리축이 존재하지 않는 경우이므로 충돌이 발생합니다.
+	return true;
+}
+
+bool MathUtils::AABBPlane(const AABB3D& aabb, const Plane3D& plane)
+{
+	// plane과 AABB간의 최대 거리를 구해줍니다.
+	float pLen =
+		aabb.size.x * fabsf(plane.normal.x) +
+		aabb.size.y * fabsf(plane.normal.y) +
+		aabb.size.z * fabsf(plane.normal.z);
+
+	// plane의 법선 벡터와 AABB 위치 벡터를 내적합니다.
+	float dot = plane.normal.Dot(aabb.position);
+
+	// plane과 AABB 중심 간의 거리와 평면이 얼마나 떨어져 있는지를 계산합니다.
+	float dist = dot - plane.distance;
+
+	// plane과 AABB 간의 거리(dist)가 pLen 값(plane과 AABB 간의 최대 거리) 이내에 있는지를 판별합니다.
+	return fabsf(dist) <= pLen;
+}
+
+bool MathUtils::OverlapOnAxis(const OBB3D& obb1, const OBB3D& obb2, const Vec3& axis)
+{
+	// OBB1를 axis 축에 투영시킨 결과를 저장합니다.
+	Interval3D a = GetInterval(obb1, axis);
+	// OBB2를 axis 축에 투영시킨 결과를 저장합니다.
+	Interval3D b = GetInterval(obb2, axis);
+
+	// 투영한 결과인 a, b가 서로 겹치는지 확인합니다.
+	return ((b.min <= a.max) && (a.min <= b.max));
+}
+
+bool MathUtils::OBBOBB(const OBB3D& obb1, const OBB3D& obb2)
+{
+	// 충돌을 테스트 하기 위한 분리 축 15개(obb1 3개, obb2 3개, obb1와 obb2의 외적 9개)를 저장합니다.
+	// * 우선은 초기값으로 aabb 3개, obb 3개의 축만 넣어주도록 합니다.
+	Vec3 test[15] =
+	{
+		obb1.orientation.Right(),		// OBB1 Axis
+		obb1.orientation.Up(),			// OBB1 Axis
+		obb1.orientation.Backward(),	// OBB1 Axis
+
+		obb2.orientation.Right(),		// OBB2 Axis
+		obb2.orientation.Up(),			// OBB2 Axis
+		obb2.orientation.Backward(),	// OBB2 Axis
+	};
+
+	// 나머지 축은 OBB1의 축과 OBB2의 축을 Cross(외적)하여 구해줍니다.
+	for (int i = 0; i < 3; i++)
+	{
+		test[6 + i * 3 + 0] = test[i].Cross(test[0]);
+		test[6 + i * 3 + 1] = test[i].Cross(test[1]);
+		test[6 + i * 3 + 2] = test[i].Cross(test[2]);
+	}
+
+	// 찾은 15개의 축들을 순회하며 분리축이 존재하는지 체크합니다.
+	for (int i = 0; i < 15; i++)
+	{
+		// 분리축(Interval이 떨어져 있는 축)이 존재한다면 충돌하지 않은 것을 의미합니다.
+		if (OverlapOnAxis(obb1, obb2, test[i]) == false)
+		{
+			return false;
+		}
+	}
+
+	// 분리축이 존재하지 않는 경우이므로 충돌이 발생합니다.
+	return true;
+}
+
+bool MathUtils::PlanePlane(const Plane3D& plane1, const Plane3D& plane2)
+{
+	return plane1.normal.Dot(plane2.normal) == 1;
+}
--- GameCoding3D/Libraries/Include/Engine/MathUtils.h
+++ GameCoding3D/Libraries/Include/Engine/MathUtils.h
@@ -3,7 +3,6 @@
 
 struct MathUtils
 {
-	/**********************************/
 	/*********** Point Test ***********/
 
 	// Sphere2Point
@@ -41,5 +40,43 @@ struct MathUtils
 	static bool PointInRay(const Point3D& point, const Ray3D& ray);
 	// * 점과 Ray 사이의 가장 가까운 점(Ray 범위 내)을 구하는 함수
 	static Point3D ClosestPoint(const Ray3D& ray, const Point3D& point);
+
+
+	/*********** Intersection ***********/
+
+	// Sphere2Sphere
+	static bool SphereSphere(const Sphere3D& s1, const Sphere3D& s2);
+	// Sphere2AABB
+	static bool SphereAABB(const Sphere3D& s, const AABB3D& aabb);
+	// Sphere2OBB
+	static bool SphereOBB(const Sphere3D& s, const OBB3D& obb);
+	// Sphere2Plane
+	static bool SpherePlane(const Sphere3D& s, const Plane3D& plane);
+
+	
+	// AABB2AABB
+	static bool AABBAABB(const AABB3D& aabb1, const AABB3D& aabb2);
+	
+	// AABB2OBB
+	// * 어떤 축에 Box를 투영시킨 결과 Interval(min, max)을 구하는 함수 2종
+	static Interval3D GetInterval(const AABB3D& aabb, const Vec3& axis);
+	static Interval3D GetInterval(const OBB3D& obb, const Vec3& axis);
+	// * 축에 따라 AABB와 OBB의 투영 결과가 겹치는지 판별하는 함수
+	static bool OverlapOnAxis(const AABB3D& aabb, const OBB3D& obb, const Vec3& axis);
+
+	static bool AABBOBB(const AABB3D& aabb, const OBB3D& obb);
+
+	// AABB2Plane
+	static bool AABBPlane(const AABB3D& aabb, const Plane3D& plane);
+
+
+	// OBB2OBB
+	// * 축에 따라 OBB와 OBB의 투영 결과가 겹치는지 판별하는 함수
+	static bool OverlapOnAxis(const OBB3D& obb1, const OBB3D& obb2, const Vec3& axis);
+	static bool OBBOBB(const OBB3D& obb1, const OBB3D& obb2);
+
+
+	// Plane2Plane
+	static bool PlanePlane(const Plane3D& plane1, const Plane3D& plane2);
 };
 
